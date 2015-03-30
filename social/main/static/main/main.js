63ea477daa14d191fa328825bcf7664e50e60fda
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-03-30 12:35:41 -0700

    pseudocode

--- social/main/static/main/main.js
+++ social/main/static/main/main.js
@@ -1,22 +1,34 @@
 /*
  Click listener for form submit in navbar
- */
-$("#submitMood").click(function () {
-    $("submitMood").blur();
-    $.post("/mood", {
-            csrfmiddlewaretoken: csrf_token,
-            data: JSON.stringify({
-                description: $("#mood-input").val()
-            })
-        },
-        function (data) {
-            console.log(data);
-            $('#moodModal').modal('hide');
+*/
 
-        }
-    );
+$("#submitMood").click(function() {
+    $("submitMood").blur();
+    postMoods($("#mood-input").val(), function(response) {
+        $('#moodModal').modal('hide');
+    });
 });
 
+$("#submitMood2").click(function() {
+    $("submitMood2").blur();
+    postMoods($("#mood-input-2").val(), function(response) {
+        // do something
+    });
+});
+
+function postMoods(data, callback) {
+    $.post("/mood", {
+            csrfmiddlewaretoken: csrf_token,
+            data: JSON.stringify(data)
+        },
+        function(response) {
+            console.log(response);
+            if (callback) {
+                callback(response);
+            }
+        }
+    );
+}
 function getMoods(username) {
     var url = "/mood";
     if (username) {
